f0d9aa98dc6644c88fc59aad1f283299429fec08
stacey <1961027+istisiki@users.noreply.github.com>
2025-07-29 11:23:17 +0800

    copy keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cf", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  -- OSC 52 sequence for remote clipboard
+  if vim.env.SSH_TTY then
+    local osc52 = require("vim.ui.clipboard.osc52")
+    osc52.copy("+")(path)
+  end
+  vim.notify("Copied to clipboard: " .. path)
+end, { desc = "Copy file path to register" })
